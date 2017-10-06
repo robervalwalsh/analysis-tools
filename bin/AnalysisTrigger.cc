ab7cd0b139a1305e41a992dfa6c78f944b56c6b4
Roberval Walsh <roberval.walsh@gmail.com>
2017-10-06 18:28:33 +0200

    Modified example how to read the prescale info

--- bin/AnalysisTrigger.cc
+++ bin/AnalysisTrigger.cc
@@ -20,28 +20,33 @@ int main(int argc, char * argv[])
    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
    
    // Input files list
-   std::string inputList = "rootFileList.txt";
+   std::string inputList = "rootfile.txt";
    Analysis analysis(inputList);
    
    // Physics Objects Collections
    analysis.triggerResults("MssmHbb/Events/TriggerResults");
-   std::string hltPath1 = "HLT_DoubleJetsC100_SingleBTagCSV_p014_v";
-   std::string hltPath2 = "HLT_DoubleJetsC100_SingleBTagCSV_p014_SinglePFJetC350_v";
+   std::string hltPath = "HLT_Mu8_v";
+   std::vector<std::string> l1seeds;
+   l1seeds.push_back("L1_SingleMu3");
+   l1seeds.push_back("L1_SingleMu5");
+   l1seeds.push_back("L1_SingleMu7");
    
-   // Trigger objects
-   std::vector<std::string> jetTriggerObjects;
-   jetTriggerObjects.push_back("hltL1sDoubleJetC100");
-   jetTriggerObjects.push_back("hltDoubleJetsC100");
-   jetTriggerObjects.push_back("hltSingleBTagCSV0p84");
-   jetTriggerObjects.push_back("hltJetC350");
-   
-   std::string trgobj_path = "MssmHbb/Events/selectedPatTrigger/";
-   for ( auto & obj : jetTriggerObjects )
-      analysis.addTree<TriggerObject>(obj,trgobj_path+obj);
+//    // Trigger objects
+//    std::vector<std::string> jetTriggerObjects;
+//    jetTriggerObjects.push_back("hltL1sDoubleJetC100");
+//    jetTriggerObjects.push_back("hltDoubleJetsC100");
+//    jetTriggerObjects.push_back("hltSingleBTagCSV0p84");
+//    jetTriggerObjects.push_back("hltJetC350");
+//    
+//    std::string trgobj_path = "MssmHbb/Events/selectedPatTrigger/";
+//    for ( auto & obj : jetTriggerObjects )
+//       analysis.addTree<TriggerObject>(obj,trgobj_path+obj);
    
    // Analysis of events
    std::cout << "This analysis has " << analysis.size() << " events" << std::endl;
-   for ( int i = 0 ; i < analysis.size() ; ++i )
+   int nevts = analysis.size();
+   nevts = 1;
+   for ( int i = 0 ; i < nevts ; ++i )
    {
       analysis.event(i);
       
@@ -50,20 +55,16 @@ int main(int argc, char * argv[])
       
       // Trigger results: fired and prescales
       // hltPath1
-      int trg1_fired = analysis.triggerResult(hltPath1);
-      int trg1_l1ps  = analysis.triggerL1Prescale(hltPath1);
-      int trg1_hltps = analysis.triggerHLTPrescale(hltPath1);
-      std::string s_accept1 = " fired ";
-      if ( ! trg1_fired ) s_accept1 = " did not fire ";
-      std::cout << "The path " << hltPath1 << s_accept1 << " and has L1 PS = " << trg1_l1ps << " and HLT PS = " << trg1_hltps << std::endl; 
+      int trg_fired = analysis.triggerResult(hltPath);
+      std::map<std::string,int> l1ps = analysis.triggerPrescale(l1seeds);
+      int hltps = analysis.triggerPrescale(hltPath);
+      std::string s_accept = " fired ";
+      if ( ! trg_fired ) s_accept = " did not fire ";
+      std::cout << "The path " << hltPath << s_accept << "and has HLT PS = " << hltps << std::endl; 
+      std::cout << "and its L1 seeds have the following prescales: " << endl;
+      for ( auto & l1 : l1seeds )
+         std::cout << "   " << l1 << ": " << l1ps[l1] << std::endl;
       
-      // hltPath2
-      int trg2_fired = analysis.triggerResult(hltPath2);
-      int trg2_l1ps  = analysis.triggerL1Prescale(hltPath2);
-      int trg2_hltps = analysis.triggerHLTPrescale(hltPath2);
-      std::string s_accept2 = " fired ";
-      if ( ! trg2_fired ) s_accept2 = " did not fire ";
-      std::cout << "The path " << hltPath2 << s_accept2 << " and has L1 PS = " << trg2_l1ps << " and HLT PS = " << trg2_hltps << std::endl; 
    }
    
 //    
