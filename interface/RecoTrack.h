146705a24d0ca7bd77094fd09d6c0255702afc52
Roberval Walsh <roberval.walsh@gmail.com>
2018-05-26 14:50:34 +0200

    Addition of member functions for RecoTracks

--- interface/RecoTrack.h
+++ interface/RecoTrack.h
@@ -31,6 +31,19 @@
 namespace analysis {
    namespace tools {
 
+      /// track quality
+      enum TrackQuality {
+          undefQuality = -1,
+          loose = 0,
+          tight = 1,
+          highPurity = 2,
+          confirmed = 3,  // means found by more than one iteration
+          goodIterative = 4,  // meaningless
+          looseSetWithPV = 5,
+          highPuritySetWithPV = 6,
+          discarded = 7, // because a better track found. kept in the collection for reference....
+          qualitySize = 8
+      };
       class RecoTrack : public Candidate {
          public:
             /// default constructor
@@ -40,10 +53,63 @@ namespace analysis {
             /// destructor
            ~RecoTrack();
             // Gets
-
+            float chi2() const;
+            float ndof() const;
+            float d0() const;
+            float dxy() const;
+            int   numberOfLostMuonHits() const;
+            int   numberOfBadMuonHits() const;
+            int   numberOfValidMuonHits() const;
+            int   numberOfValidTrackerHits() const;
+            int   numberOfValidStripTECHits() const;
+            int   numberOfValidStripTIBHits() const;
+            int   numberOfValidStripTIDHits() const;
+            int   numberOfValidStripTOBHits() const;
+            int   muonStationsWithValidHits() const;
+            int   muonStationsWithBadHits() const;
+            int   innermostMuonStationWithValidHits() const;
+            int   outermostMuonStationWithValidHits() const;
+            bool  quality(const TrackQuality & trkqual);
+            
             // Sets
+            void chi2(const float &);
+            void ndof(const float &);
+            void d0(const float &);
+            void dxy(const float &);
+            void numberOfLostMuonHits(const int &);
+            void numberOfBadMuonHits(const int &);
+            void numberOfValidMuonHits(const int &);
+            void numberOfValidTrackerHits(const int &);
+            void numberOfValidStripTECHits(const int &);
+            void numberOfValidStripTIBHits(const int &);
+            void numberOfValidStripTIDHits(const int &);
+            void numberOfValidStripTOBHits(const int &);
+            void muonStationsWithValidHits(const int &);
+            void muonStationsWithBadHits(const int &);
+            void innermostMuonStationWithValidHits(const int &);
+            void outermostMuonStationWithValidHits(const int &);
+            void quality(const TrackQuality & , const bool &);
+            
+            
           protected:
           // ----------member data ---------------------------
+          float chi2_;
+          float ndof_;
+          float d0_;
+          float dxy_;
+          std::map<TrackQuality,bool> qual_;
+          int   nLostMuHits_;
+          int   nBadMuHits_;
+          int   nValMuHits_;
+          int   nValTrackerHits_;
+          int   nValStripTECHits_;
+          int   nValStripTIBHits_;
+          int   nValStripTIDHits_;
+          int   nValStripTOBHits_;
+          int   muStationsWithValHits_;
+          int   muStationsWithBadHits_;
+          int   inMuStationWithValHits_;
+          int   outMuStationWithValHits_;
 
          private:
          // ----------member data ---------------------------
--- src/RecoTrack.cc
+++ src/RecoTrack.cc
@@ -50,7 +50,41 @@ RecoTrack::~RecoTrack()
 //
 
 // Gets 
+float RecoTrack::chi2()                                const { return chi2_;                    }
+float RecoTrack::ndof()                                const { return ndof_;                    }
+float RecoTrack::d0()                                  const { return d0_;                      }
+float RecoTrack::dxy()                                 const { return dxy_;                     }
+int   RecoTrack::numberOfLostMuonHits()                const { return nLostMuHits_;             }
+int   RecoTrack::numberOfBadMuonHits()                 const { return nBadMuHits_;              }
+int   RecoTrack::numberOfValidMuonHits()               const { return nValMuHits_;              }
+int   RecoTrack::numberOfValidTrackerHits()            const { return nValTrackerHits_;         }
+int   RecoTrack::numberOfValidStripTECHits()           const { return nValStripTECHits_;        }
+int   RecoTrack::numberOfValidStripTIBHits()           const { return nValStripTIBHits_;        }
+int   RecoTrack::numberOfValidStripTIDHits()           const { return nValStripTIDHits_;        }
+int   RecoTrack::numberOfValidStripTOBHits()           const { return nValStripTOBHits_;        }
+int   RecoTrack::muonStationsWithValidHits()           const { return muStationsWithValHits_;   }
+int   RecoTrack::muonStationsWithBadHits()             const { return muStationsWithBadHits_;   }
+int   RecoTrack::innermostMuonStationWithValidHits()   const { return inMuStationWithValHits_;  }
+int   RecoTrack::outermostMuonStationWithValidHits()   const { return outMuStationWithValHits_; }
+bool  RecoTrack::quality(const TrackQuality & trkqual) { return qual_[trkqual];           }
 
 // Sets
+void RecoTrack::chi2(const float & chi2)                          { chi2_ = chi2;                 }
+void RecoTrack::ndof(const float & ndof)                          { ndof_ = ndof;                 }
+void RecoTrack::d0(const float & d0  )                            { d0_   = d0;                   }
+void RecoTrack::dxy(const float & dxy )                           { dxy_  = dxy;                  }
+void RecoTrack::numberOfLostMuonHits(const int & n)               { nLostMuHits_ = n;             }
+void RecoTrack::numberOfBadMuonHits(const int & n)                { nBadMuHits_ = n;              }
+void RecoTrack::numberOfValidMuonHits(const int & n)              { nValMuHits_ = n;              }
+void RecoTrack::numberOfValidTrackerHits(const int & n)           { nValTrackerHits_ = n;         }
+void RecoTrack::numberOfValidStripTECHits(const int & n)          { nValStripTECHits_ = n;        }
+void RecoTrack::numberOfValidStripTIBHits(const int & n)          { nValStripTIBHits_ = n;        }
+void RecoTrack::numberOfValidStripTIDHits(const int & n)          { nValStripTIDHits_ = n;        }
+void RecoTrack::numberOfValidStripTOBHits(const int & n)          { nValStripTOBHits_ = n;        }
+void RecoTrack::muonStationsWithValidHits(const int & n)          { muStationsWithValHits_ = n;   }
+void RecoTrack::muonStationsWithBadHits(const int & n)            { muStationsWithBadHits_ = n;   }
+void RecoTrack::innermostMuonStationWithValidHits(const int & n)  { inMuStationWithValHits_ = n;  }
+void RecoTrack::outermostMuonStationWithValidHits(const int & n)  { outMuStationWithValHits_ = n; }
+void RecoTrack::quality(const TrackQuality & trkqual, const bool & status)  {  qual_[trkqual] = status; }
 
 
