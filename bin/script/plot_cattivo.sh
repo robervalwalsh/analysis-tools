89543e99386a287216c66020fc1d286a93a7ddd5
Fabio Zoratti <fabio.zoratti96@gmail.com>
2018-08-02 17:09:32 +0200

    Updated plotter

--- bin/script/plot_cattivo.sh
+++ bin/script/plot_cattivo.sh
@@ -2,25 +2,22 @@
 # This script creates all the possible plots with
 # all types of correction level for the AnalysisJetsSmrBTagRegFSR.cc
 
-CORRECTION_LEVELS=(nothing only_smearing smearing_btag smearing_btag_regression smearing_btag_regression_fsr)
-TRIGGER_FLAGS=("true" "false")
+CORRECTION_LEVELS=(nothing_false only_smearing_false smearing_btag_false smearing_btag_regression_false smearing_btag_regression_fsr_false smearing_btag_regression_fsr_true)
 TYPE=("lep" "chr")
 MASS_POINTS=(120 350 1200)
 OUTPUT_DIR="output/hists/jets"
 
 for mass in ${MASS_POINTS[@]} ; do
     for corr in ${CORRECTION_LEVELS[@]} ; do
-        for trig in ${TRIGGER_FLAGS[@]} ; do
-            for l in ${TYPE[@]} ; do
-                FILE_NAME="${OUTPUT_DIR}/before_new_mass_${mass}/${corr}_${trig}"
-                echo "Doing ${FILE_NAME}"
-                cd ..
-                PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}.png" &
-                PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}.pdf" &
-                PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}.eps" &
-                PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}_root.root" &
-                cd output
-            done
+        for l in ${TYPE[@]} ; do
+            FILE_NAME="${OUTPUT_DIR}/new_mass_${mass}/${corr}"
+            echo "Doing ${FILE_NAME}"
+            cd ..
+            PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}.png" &
+            PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}.pdf" &
+            PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}.eps" &
+            PlotStackStyle "${FILE_NAME}_${l}.json" "${FILE_NAME}_${l}_root.root" &
+            cd script
         done
     done
 done
