c3d4d0e0d3b40f17aa5abdf56a723f954961e2b6
Fabio Zoratti <fabio.zoratti96@gmail.com>
2018-08-08 17:23:27 +0200

    Improved parallelization for bkg

--- bin/script/run_all_bkg.sh
+++ bin/script/run_all_bkg.sh
@@ -2,12 +2,14 @@
 
 CORRECTION_LEVELS=(nothing_false regression_false regression_fsr_false regression_fsr_true)
 OUTPUT_DIR_BASE="output/hists/bkg"
+MAX_N=12
+OUTPUT_DIR="$OUTPUT_DIR_BASE/test_new"
+
 
 hash -r
 for corr in ${CORRECTION_LEVELS[@]} ; do
-    OUTPUT_DIR="$OUTPUT_DIR_BASE/test"
     mkdir -p ${OUTPUT_DIR}
-    for i in $(seq 0 22) ; do 
+    for i in $(seq 0 ${MAX_N}) ; do 
         EXECUTABLE="bkg_${corr}_${i}"
         OUT_FILE="${OUTPUT_DIR}/${corr}_${i}.root"
         if ! [[ -f ${OUT_FILE} ]] ; then
@@ -18,5 +20,15 @@ for corr in ${CORRECTION_LEVELS[@]} ; do
     done
 done
 
+
 wait
-echo -e "\n\nAll process exited.\n\nSuccess.\n\n"
+echo -e "\n\nAll process exited. Now I merge"
+
+for corr in ${CORRECTION_LEVELS[@]} ; do
+    INFILES=$(ls ${OUTPUT_DIR} | grep ${corr})
+    hadd ${corr}.root $INFILES
+    rm $INFILES
+done
+
+echo -e "\n\nMerged everything"
+        
