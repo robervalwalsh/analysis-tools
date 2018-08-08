150a633dd8cb9a5fdd7b46e576e39e6b99f9e777
Fabio Zoratti <fabio.zoratti96@gmail.com>
2018-08-08 17:18:18 +0200

    Background fit working. Sometimes

--- bin/script/fittone_bkg.py
+++ bin/script/fittone_bkg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
 
 from ROOT import TH1F, TFile, TMath, TF1, TH1F, TCanvas
@@ -6,6 +6,7 @@ from math import erf
 
 import numpy
 import os
+import pylab
 
 directory = '../output/hists/bkg/test'
 
@@ -17,19 +18,22 @@ correction_level = ["nothing_false",
 ]
 
 
-def activ(x, par):
+def activ(x, p):
     return 0.5*(1 + erf(p[0]*(x[0] - p[1])))
 
 
-def second(x, par):
+def second(x, p):
+    if p[2] == 0 or p[3] == 0:
+        return 10**20
     chi = 2*TMath.Sqrt(TMath.Log(4))
-    sig0 = 2/chi*TMath.ArSinh(p[3]*chi/2)
-    inside = 1 - (x[0] - p[1])*p[3]/p[2] - (x[0] - p[1])**2*p[3]*p[4]/p[3]
+    sig0 = 2/chi*TMath.ASinH(p[3]*chi/2)
+    inside = 1 - (x[0] - p[1])*p[3]/p[2] - (x[0] - p[1])**2*p[3]*p[4]/p[2]
     return p[0]*TMath.Exp(-0.5*((TMath.Log(inside)/sig0)**2 + sig0**2))
 
 
 def super_novosibirisk(x, par):
-    return activ(x, par[:2])*second(x, par[-5:])
+    return activ(x, [par[0], par[1]])*second(
+        x, [par[2], par[3], par[4], par[5], par[6]])
 
 
 def prendi_parametri(funzione):
@@ -46,18 +50,22 @@ def scrivi_parametri(params, errors, fileout):
         fileout.write(str(p) + "\t" + str(e) + "\t")
 
 
-canvases = ["" for i in range(0, len(mass_points)*len(correction_level)*len(lep))]
+canvases = ["" for i in range(0, len(correction_level)*len(lep))]
 i = 0
+
+
+limit_string = "Mass < 800"
+
 
 for l in lep:
     cur_dir = directory
-    output_filename = os.path.join("../output/tables/bkg/test/",
+    output_filename = os.path.join("../output/tables/bkg/test_new/",
                                    "_".join(["fit", l]) + ".txt")
     out_file = open(output_filename, "w")
     out_file.write("Intestazione")
     for c in correction_level:
-        canvases[i] = TCanvas("_".join([c, l, mass, "2 match"]),
-                              " ".join([c, l, mass, "2 match"]))
+        canvases[i] = TCanvas("_".join([c, l]),
+                              " ".join([c, l]))
         out_file.write("\n")
         input_filename = c + ".root"
         input_filename = os.path.join(cur_dir, input_filename)
@@ -72,21 +80,34 @@ for l in lep:
             lc = "chromo"
             filter_string = "!(Leptonic_event)"
         funzione = TF1("novosibirisk", super_novosibirisk, 0, 800, 7)
-        max_x = 1.1*limits[mass][1]
-        min_x = 0.9*limits[mass][0]
+        limits_up = 800
+        limits_down = 0
+        max_x = 1.1*limits_up
+        min_x = 0.9*limits_down
         bins = 60
         binsize = (max_x - min_x)/bins
         histo = TH1F("histo", "Fittone", bins, min_x, max_x)
         canvases[i].cd()
         tree.Draw("Mass>>histo", " && ".join([filter_string, ]), "norm")
         histo.Scale(1./binsize)
-        histo.Fit(funzione_test, "RLME", "", limits[mass][0], limits[mass][1])
+
+        funzione.SetParLimits(0, 0, 0.1)
+        funzione.SetParLimits(3, 0, 800)
+        funzione.SetParameters(.03, 100, 0.006, 110, 5000, 0.1, 0.1)
+        funzione.Draw("same")
+        # input("cusumano")
+        
+        histo.Fit(funzione, "RLME", "", limits_down, limits_up)
+        funzione.DrawClone("same")
         params, errors = prendi_parametri(funzione)
         print(params)
         funzione.SetParameters(*params)
         fit = tree.UnbinnedFit(
-            "gaus1", "Mass",
+            "novosibirisk", "Mass",
             " && ".join([limit_string, filter_string]), "RLME")
         params, errors = prendi_parametri(funzione)
         funzione.DrawClone("same")
         scrivi_parametri(params, errors, out_file)
+        input("test")
+
+
