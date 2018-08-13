a511b2c7895059b1ee0abe2a413d2b7091eba8c2
Fabio Zoratti <fabio.zoratti96@gmail.com>
2018-08-13 11:49:08 +0200

    Now the heavy jobs are run on the HTCondor

--- bin/script/run_all.py
+++ bin/script/run_all.py
@@ -4,48 +4,74 @@
 
 from settings_parallelization import correction_level_bkg, correction_level_signal, \
     mass_points_signal, bkg_files, split_list
-from subprocess import Popen, STDOUT
+from subprocess import Popen, STDOUT, PIPE
 from time import sleep
+from jinja2 import FileSystemLoader, Environment
+template_loader = FileSystemLoader(searchpath='./script/templates')
+template_env = Environment(loader=template_loader)
+template_exec = template_env.get_template("exe.j2")
+template_submit = template_env.get_template("htc_sub.j2")
 
 import os
 import sys
 
 DEVNULL = open(os.devnull, "wb")
-
+condor_script_executable = "../scripts/htc_sub.sh"
 process_list = list()
 
-if sys.argv[1] == "bkg":
-    output_dir = "output/hists/bkg/third"
+
+def condor_submit(executable, args, name, runtime=50000, memory=1000):
+    out_text = template_exec.render(
+        executable=executable, arg_list=args)
+    filename = name + ".sh"
+    filename = os.path.join("_tmp/condor/", filename)
+    fileout = open(filename, "w")
+    fileout.write(out_text)
+    fileout.close()
+    os.chmod(filename, 0755)
+    process_list.append(
+        {'filename': filename, 'runtime': str(runtime),
+         'memory': str(memory)})
+
+    
+if sys.argv[1] == "bkg" and len(sys.argv) == 2:
+    output_dir = "output/hists/bkg/fourth"
     bins = 100
     highx = 800
-    
     for params in bkg_files:
         for cl in correction_level_bkg:
             executable = "bkg_" + "_".join(cl)
             output_file = "_".join([params['mass'], params['era'], cl[0], cl[1]])
             output_file = os.path.join(output_dir, output_file)
-            for lista, i in zip(split_list(params['filenames'], 8), range(0, 10000)):
-                tmp_output_file = output_file + "_" + str(i) + ".root"
+            for lista, i in zip(split_list(params['filenames'], 5), range(0, 10000)):
+                tmp_output_file = "_".join([output_file, params["era"], str(i)]) + ".root"
                 veralista = [os.path.join(params['basedir'], l) for l in lista ]
-                process_list.append(Popen([executable, tmp_output_file,
-                                           str(bins), str(highx)] + veralista,
-                                          stdout=DEVNULL, stderr=STDOUT))
-            print("Now I wait a bit")
-            sleep(20)
-    print("All process launched.")
-    appo = [p.wait() for p in process_list]
-    print("All process exited. Now i merge the result")
+                condor_submit(
+                    executable,
+                    [tmp_output_file, str(bins), str(highx)] + veralista,
+                    "_".join([executable, params["era"], str(i)]), 80000, 1000)
+            
+    bash_filename = "_tmp/run_condor.sh"
+    bash_file = open(bash_filename, "w")
+    bash_file.write("#!/bin/bash\n")
+    for p in process_list:
+        bash_file.write(" ".join([condor_script_executable,
+                                  p['filename'], p['runtime'], p['memory']]) + "\n")
+    os.chmod(bash_filename, 0755)
+    proc = Popen([bash_filename, ], stdout=PIPE)
+    proc.wait()
+elif sys.argv[1] == "bkg" and sys.argv[2] is not None and sys.argv[2] == "merge":
+    output_dir = "output/hists/bkg/fourth"
     for params in bkg_files:
         for cl in correction_level_bkg:
             output_file = "_".join([params['mass'], params['era'], cl[0], cl[1]])
             output_file = os.path.join(output_dir, output_file)
             to_merge = list()
-            for i in range(0, len(split_list(params['filenames'], 10))):
-                to_merge.append(output_file + "_" + str(i) + ".root")
-            output_file += ".root"
-            Popen(["hadd", output_file] + to_merge)
-            Popen(["rm"] + to_merge)
-    print("Merged")
+            for lista, i in zip(split_list(params['filenames'], 5), range(0, 10000)):
+                to_merge.append("_".join([output_file, params["era"], str(i)]) + ".root")
+            process = Popen(["hadd", "-f", output_file + ".root"] + to_merge, stdout=PIPE)
+            process.wait()
+            # Here we do rm if all is ok
             
 elif sys.argv[1] == "signal":
     output_dir = "output/hists/jets/last"
