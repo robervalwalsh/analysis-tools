df3909fb3a78d813986ef02cc1b04ca51a460b19
Roberval Walsh <roberval.walsh@gmail.com>
2018-09-06 15:53:02 +0200

    improved formatting

--- bin/Cutflow.cc
+++ bin/Cutflow.cc
@@ -18,9 +18,9 @@ int main(int argc, char * argv[])
    TH1F * h = (TH1F*) f.Get("cutflow");
    
    
-   printf("+%s+\n", std::string(110,'-').c_str());
-   printf("| %-48s |    %10s |   %16s |   %16s |\n","cut","n events","ratio wrt first","ratio wrt previous");
-   printf("+%s+\n", std::string(110,'-').c_str());
+   printf("+%s+\n", std::string(150,'-').c_str());
+   printf("| %-88s |    %10s |   %16s |   %16s |\n","cut","n events","ratio wrt first","ratio wrt previous");
+   printf("+%s+\n", std::string(150,'-').c_str());
    int firstbin = -1;
    for ( int i = 1; i <= h ->GetNbinsX(); ++i )
    {
@@ -32,16 +32,16 @@ int main(int argc, char * argv[])
       float rni = 0;
       if ( i == firstbin )
       {
-         printf("| %-48s |    %10.0f |   %16.4f |  %19s |\n",label.c_str(),n,rn1,"-");
+         printf("| %-88s |    %10.0f |   %16.4f |  %19s |\n",label.c_str(),n,rn1,"-");
       }
       else
       {
          rni = h-> GetBinContent(i)/h->GetBinContent(i-1);
-         printf("| %-48s |    %10.0f |   %16.4f |     %16.4f |\n",label.c_str(),n,rn1,rni);
+         printf("| %-88s |    %10.0f |   %16.4f |     %16.4f |\n",label.c_str(),n,rn1,rni);
       }
       
    }
-   printf("+%s+\n", std::string(110,'-').c_str());
+   printf("+%s+\n", std::string(150,'-').c_str());
 
    return 0;
    
