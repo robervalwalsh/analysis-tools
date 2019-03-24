07ac1e612a39b43df9148914b2a43ca6b8705684
Roberval Walsh <roberval.walsh@gmail.com>
2019-03-24 12:57:58 +0100

    example retrieving trigger objects

--- bin/AnalyserTriggerExample.cc
+++ bin/AnalyserTriggerExample.cc
@@ -16,6 +16,14 @@ int main(int argc, char ** argv)
       // trigger selection
       if ( ! trigger.selectionHLT() )   continue;
       if ( ! trigger.selectionL1 () )   continue;  // to be used in case of "OR" of seeds
+      
+      // L1 jets trigger objects
+      auto l1jets = trigger.triggerObjectsL1Jets();
+      for ( auto jet : l1jets )
+      {
+         std::cout << jet->pt() << std::endl;
+      }
+      std::cout << "----" << std::endl;
    }
    
    return 0;
