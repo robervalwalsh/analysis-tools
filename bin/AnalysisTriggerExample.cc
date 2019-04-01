c3650b5e7196a348f43d5f6df3ba8564a337a554
Roberval Walsh <roberval.walsh@gmail.com>
2019-04-01 09:35:12 +0200

    update to include parameters passed by config

--- bin/AnalysisTriggerExample.cc
+++ bin/AnalysisTriggerExample.cc
@@ -8,6 +8,7 @@
 #include "TH1.h" 
 
 #include "Analysis/Tools/interface/Analysis.h"
+#include "Analysis/Tools/bin/macro_config.h"
 
 using namespace std;
 using namespace analysis;
@@ -19,32 +20,22 @@ int main(int argc, char * argv[])
 {
    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
    
+   if ( macro_config(argc, argv) != 0 ) return -1;
+   
    // Input files list
-   std::string inputList = "rootFileList.txt";
-   Analysis analysis(inputList);
+   Analysis analysis(inputlist_);
    
    // Trigger
-   analysis.triggerResults("MssmHbb/Events/TriggerResults");
-   std::string hltPath = "HLT_DoublePFJets100MaxDeta1p6_DoubleCaloBTagCSV_p33_v";
-   std::vector<std::string> l1seeds;  // in case there is an OR of L1 seeds
-   l1seeds.push_back("L1_DoubleJet100er2p3_dEta_Max1p6");
-   l1seeds.push_back("L1_DoubleJet112er2p3_dEta_Max1p6");
+   analysis.triggerResults(triggerCol_);
    
    // Trigger objects
-   std::vector<std::string> jetTriggerObjects;
-   jetTriggerObjects.push_back("hltL1DoubleJet100er2p3dEtaMax1p6Ior112er2p3dEtaMax1p6");
-   jetTriggerObjects.push_back("hltDoubleCaloBJets100eta2p3");
-   jetTriggerObjects.push_back("hltDoublePFJets100Eta2p3");
-   jetTriggerObjects.push_back("hltBTagCalo80x6CSVp0p92DoubleWithMatching");
-   
-   std::string trgobj_path = "MssmHbb/Events/slimmedPatTrigger";
-   for ( auto & obj : jetTriggerObjects )
-      analysis.addTree<TriggerObject>(obj,trgobj_path+"/"+obj);
+   for ( auto & obj : triggerObjectsJets_ )
+      analysis.addTree<TriggerObject>(obj,triggerObjDir_+"/"+obj);
    
    // Analysis of events
    std::cout << "This analysis has " << analysis.size() << " events" << std::endl;
    int nevts = analysis.size();
-   nevts = 10;
+   if ( nevts > 0 ) nevts = nevtmax_;
    for ( int i = 0 ; i < nevts ; ++i )
    {
       analysis.event(i);
@@ -53,21 +44,25 @@ int main(int argc, char * argv[])
       
       // Trigger results: fired and prescales
       // hltPath 
-      int trg_fired = analysis.triggerResult(hltPath);
+      int trg_fired = analysis.triggerResult(hltPath_);
       // prescales
-      std::map<std::string,int> l1ps = analysis.triggerPrescale(l1seeds);
-      int hltps = analysis.triggerPrescale(hltPath);
+      int l1ps  = analysis.triggerPrescale(l1Seed_);
+      int hltps = analysis.triggerPrescale(hltPath_);
       std::string s_accept = " fired ";
       
       // printing info
-      if ( ! trg_fired ) s_accept = " did not fire ";
-      std::cout << "The path " << hltPath << s_accept << "and has HLT PS = " << hltps << std::endl; 
-      std::cout << "and its L1 seeds have the following prescales: " << endl;
-      for ( auto & l1 : l1seeds )
-         std::cout << "   " << l1 << ": " << l1ps[l1] << std::endl;
+      if ( ! trg_fired )
+      {
+         std::cout << "The path " << hltPath_ << " did not fire " << std::endl; 
+         continue;
+      }
+      
+      std::cout << "The path " << hltPath_ << " fired and has HLT PS = " << hltps << std::endl; 
+      std::cout << "and its L1 seed " << l1Seed_ << " has the following prescale: " << l1ps << endl;
       
       // dealing with trigger objects
-      auto l1jets = analysis.collection<TriggerObject>("hltL1DoubleJet100er2p3dEtaMax1p6Ior112er2p3dEtaMax1p6");
+      // *** WARNING! *** the order you enter the trigger object in the config file matter!
+      auto l1jets = analysis.collection<TriggerObject>(triggerObjectsJets_[0]);
       for ( int j = 0 ; j < l1jets->size() ; ++j )
       {
          TriggerObject l1jet = l1jets->at(j);
