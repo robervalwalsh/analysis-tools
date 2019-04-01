05550b677ff42e9f63d303354c241c147a04a4d8
Roberval Walsh <roberval.walsh@gmail.com>
2019-04-01 09:45:10 +0200

    renamed

--- bin/AnalysisBJets.cc
+++ bin/AnalysisBJets.cc
@@ -95,3 +95,31 @@ float btagMin(const string & wp)
    return min;
    
 }
+
+
+// // Below is a snippet of how to use the BTV standalone classes
+// 
+// // before the event loop
+//    // b-tag scale factors
+//    BTagCalibration calib(btagalgo_, btagsf_ );
+//    BTagCalibrationReader reader(BTagEntry::OP_TIGHT,   // operating point - example for TIGHT
+//                                 "central",             // central sys type
+//                                 {"up", "down"});       // other sys types
+//    reader.load(calib,             // calibration instance
+//             BTagEntry::FLAV_B,    // btag flavour - B
+//             "comb");              // measurement type   
+//    reader.load(calib,             // calibration instance
+//             BTagEntry::FLAV_C,    // btag flavour - C
+//             "comb");              // measurement type   
+//    reader.load(calib,             // calibration instance
+//             BTagEntry::FLAV_UDSG, // btag flavour - UDSG
+//             "incl");              // measurement type   
+//    
+// // inside the event loop
+//    
+//          // b-tag scale factors
+//          double jet_bscalefactor;
+//          if ( jet.flavour("Hadron") == 5 ) jet_bscalefactor = reader.eval_auto_bounds("central", BTagEntry::FLAV_B,    fabs(jet.eta()), jet.pt() ); 
+//          if ( jet.flavour("Hadron") == 4 ) jet_bscalefactor = reader.eval_auto_bounds("central", BTagEntry::FLAV_C,    fabs(jet.eta()), jet.pt() ); 
+//          if ( jet.flavour("Hadron") == 0 ) jet_bscalefactor = reader.eval_auto_bounds("central", BTagEntry::FLAV_UDSG, fabs(jet.eta()), jet.pt() ); 
+//    
