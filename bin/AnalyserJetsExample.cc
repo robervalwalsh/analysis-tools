b5600983fd601a7a7b04f86ce47f25e336289dee
Roberval Walsh <roberval.walsh@gmail.com>
2019-08-12 14:59:27 +0200

    improvements

--- bin/AnalyserJetsExample.cc
+++ bin/AnalyserJetsExample.cc
@@ -9,40 +9,32 @@ using namespace analysis::tools;
 
 int main(int argc, char ** argv)
 {
-   TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
-   
    Analyser analyser(argc,argv);
    
-// HISTOGRAMS   
+// HISTOGRAMS definitions  
    // create some predefined jet histograms
    // if not defined, the number of jets is nJetMin from the configurations
    analyser.jetHistograms("selection01");
-   analyser.jetHistograms("selection02");
-   
-   // you can always get the ususal analysis class
-   auto analysis = analyser.analysis();
    
    for ( int i = 0 ; i < analyser.nEvents() ; ++i )
    {
       if ( ! analyser.event(i)                  )   continue;
       
    // JETS
-      // jet energy resolution to all selected jets
-      analyser.actionApplyJER();
-      // jet identification selection
-      if ( ! analyser.selectionJetId()          )   continue;
+      analyser.actionApplyJER();                               // correction : jet energy resolution smearing
+      analyser.actionApplyBjetRegression();                    // correction : jet energy regression (for b jets)
+      if ( ! analyser.selectionJetId()          )   continue;  // selection  : jet identification 
       if ( ! analyser.selectionJetPileupId()    )   continue;
-      // number of jets selection
-      if ( ! analyser.selectionNJets()          )   continue;
-      //  1st and 2nd jet kinematic selection, pt and eta
-      if ( ! analyser.selectionJet(1)           )   continue;
-      if ( ! analyser.selectionJet(2)           )   continue;
-      // delta phi jet selection
-//      if ( ! analyser.selectionJetDphi(1,2)    )   continue;
+      if ( ! analyser.selectionNJets()          )   continue;  // selection  : number of jets 
+      if ( ! analyser.selectionJet(1)           )   continue;  // selection  : jet1 pt and eta 
+      if ( ! analyser.selectionJet(2)           )   continue;  // selection  : jet2 pt and eta 
+      if ( ! analyser.selectionJetDphi(1,2)     )   continue;  // selection  : delta_phi_jets (1,2) [or  MIN(neg): analyser.selectionJetDphi(1,2,-2.0) / MAX(pos): analyser.selectionJetDphi(1,2,+2.0)]
 // HISTOGRAMS
-      analyser.fillJetHistograms("selection01");
+      analyser.fillJetHistograms("selection01");               // histograms : jets fill
       
       
    }  //end event loop
+   
+
 } // end main
       
