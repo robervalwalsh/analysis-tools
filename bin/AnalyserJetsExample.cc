224f9edb90b351d59c542cf52c5917fa29bcbd8b
Roberval Walsh <roberval.walsh@gmail.com>
2019-08-12 15:42:12 +0200

    minor things

--- bin/AnalyserJetsExample.cc
+++ bin/AnalyserJetsExample.cc
@@ -10,6 +10,7 @@ using namespace analysis::tools;
 int main(int argc, char ** argv)
 {
    Analyser analyser(argc,argv);
+   auto config = analyser.config();
    
 // HISTOGRAMS definitions  
    // create some predefined jet histograms
@@ -24,14 +25,13 @@ int main(int argc, char ** argv)
       analyser.actionApplyJER();                               // correction : jet energy resolution smearing
       analyser.actionApplyBjetRegression();                    // correction : jet energy regression (for b jets)
       if ( ! analyser.selectionJetId()          )   continue;  // selection  : jet identification 
-      if ( ! analyser.selectionJetPileupId()    )   continue;
+      if ( ! analyser.selectionJetPileupId()    )   continue;  // selection  : jet Pileup identification 
       if ( ! analyser.selectionNJets()          )   continue;  // selection  : number of jets 
       if ( ! analyser.selectionJet(1)           )   continue;  // selection  : jet1 pt and eta 
       if ( ! analyser.selectionJet(2)           )   continue;  // selection  : jet2 pt and eta 
       if ( ! analyser.selectionJetDphi(1,2)     )   continue;  // selection  : delta_phi_jets (1,2) [or  MIN(neg): analyser.selectionJetDphi(1,2,-2.0) / MAX(pos): analyser.selectionJetDphi(1,2,+2.0)]
 // HISTOGRAMS
       analyser.fillJetHistograms("selection01");               // histograms : jets fill
-      
       
    }  //end event loop
    
