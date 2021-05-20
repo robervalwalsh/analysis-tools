40b018ce557d245bc56be691bc2a3698b028f5e9
Roberval Walsh <roberval.walsh@gmail.com>
2021-05-20 13:17:24 +0200

    using yaml instead of json

--- scripts/trigger_info.py
+++ scripts/trigger_info.py
@@ -4,7 +4,8 @@ from argparse import HelpFormatter
 
 import os
 from glob import glob
-import json
+#import json
+import yaml
 from colors import bcolors
 
 red = bcolors.FAIL
@@ -13,24 +14,25 @@ bold = bcolors.BOLD
 endc = bcolors.ENDC
 
 
-def list_paths(info):
+def list_paths(info, show=False):
+   paths = []
    for path, path_info in sorted(info.items()):
-      print(f'  -> {path}')
-   
+      paths.append(path)
+      if show:
+         print(f'  -> {path}')
+   return paths
 
 def list_path_info(info,path):
    print(f'   {magenta}{bold}Path:{endc} {path}')
    l1seeds = info[path]['l1seeds']
-#   print('   L1 seeds: ' + ', '.join([s for s in sorted(l1seeds)]))
    print(f'   {magenta}{bold}L1 Seeds:{endc}')
    for seed in sorted(l1seeds):
       print(f'     - {seed}')
-   trgobjs = info[path]['trgobjs']
+   trgobjs = info[path]['trigger_objects']
    print(f'   {magenta}{bold}Trigger objects:{endc}')
    for obj in trgobjs:
       print(f'     - {obj}')
    
-
 def list_versions(vdir):
    samples = glob(f'{vdir}/mssmhbb-{year}-v*')
    samples = [os.path.basename(s) for s in samples]
@@ -46,6 +48,7 @@ parser = ArgumentParser(prog='trigger_info.py', formatter_class=lambda prog: Hel
 parser.add_argument(dest="year"                                         , help="Year of data taking  (REQUIRED)")
 parser.add_argument("-v"  , dest="version"                              , help="Ntuple production version")
 parser.add_argument("-p"  , dest="path"                                 , help="HLT path")
+parser.add_argument("-a"  , dest="all"        , action='store_true'     , help="Dump YAML file")
 
 args = parser.parse_args()
 
@@ -65,7 +68,12 @@ if not version or version == 'latest' or version == '-1':
    else:
       version = vs[-1]
 
-info_file  = f'{ntp_dir}/mssmhbb-{year}-v{version}/trigger_info.json'
+info_file  = f'{ntp_dir}/mssmhbb-{year}-v{version}/trigger_info.yml'
+
+if args.all:
+   cmd = f'cat {info_file}'
+   os.system(cmd)
+   os.sys.exit()
 
 if not os.path.isfile(info_file):
    print()
@@ -75,18 +83,19 @@ if not os.path.isfile(info_file):
    os.sys.exit()
 
 print('')
-print('--------------------------------------------------------')
-print(f' Trigger information for {year} ntuples v{version}')
-print('--------------------------------------------------------')
+print('#--------------------------------------------------------')
+print(f'# Trigger information for {year} ntuples v{version}')
+print('#--------------------------------------------------------')
+print('')
 
 with open(info_file) as f:
-   info_data = json.load(f)
+   info_data = yaml.safe_load(f)
 
 if not args.path or args.path=='':
    print(f'{magenta}{bold}Listing all paths available:{endc}')
-   list_paths(info_data)
+   list_paths(info_data,show=True)
 else:
    list_path_info(info_data,args.path)
-   
-print('--------------------------------------------------------')
+
+print('#--------------------------------------------------------')
 print('')
